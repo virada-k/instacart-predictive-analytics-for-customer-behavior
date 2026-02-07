d5d286ff4ecbe48460773ee59b83b1862622ff7a
virada-k <virada.kumpiranont@gmail.com>
2026-02-07 16:07:40 +0700

    Update and rename reorder.python to reorder.py

---
+++ 03-machine-learning/repeat-order-python/reorder.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report, roc_auc_score
+import gdown
